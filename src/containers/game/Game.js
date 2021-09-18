4a67ea83cca7a415dce130be0b9f901bc868df09
aryan2gadnevia <aryan.gandevia@gmail.com>
2021-09-18 18:05:46 -0400

    game in POGress
    gg

--- src/containers/game/Game.js
+++ src/containers/game/Game.js
@@ -1,20 +1,40 @@
+import { color } from '@chakra-ui/styled-system';
 import React from 'react'
 import Sketch from 'react-p5'
 
 function Game() {
-    const WIDTH = 800;
-    const HEIGHT = 500;
+    //width and height of monitor
+    const WIDTH = 700;
+    const HEIGHT = 700;
     
+    //background variables
+    var c1,c2
+
+    //ball varaibles
     var ballXVel = 5;
     var ballYVel = 5;
-    var ballXPos = 0;
-    var ballYPos = 0;
+    var ballXPos = WIDTH/2;
+    var ballYPos = HEIGHT/2;
+
+    //player1 variables
+    var p1X=10;
+    var p1Y=350;
+
+    //player2 variables
+
+
+    //player3 variables
+
+
+    //player4 variables
+
+
 
     function updateBallPosition() {
-        if (ballXPos > WIDTH || ballXPos < 0){
+        if (ballXPos > WIDTH-10 || ballXPos < 10){
             ballXVel *= -1;
         }
-        if (ballYPos > HEIGHT || ballYPos < 0){
+        if (ballYPos > HEIGHT-10 || ballYPos < 0){
             ballYVel *= -1;
         }
         ballXPos += ballXVel;
@@ -26,8 +46,19 @@ function Game() {
     }
 
     const draw = p5 => {
-        p5.background(255, 130, 20);
-        p5.ellipse(ballXPos, ballYPos, 100);
+        //bg
+        p5.noStroke();
+        p5.background(222,255,252);
+
+        p5.rect(10, 200, 50,50);
+
+        //ball
+        p5.ellipse(ballXPos, ballYPos, 20);
+
+        //player1 paddle
+        p5.rect(p1X,p1Y, 10,100);
+
+        //movement
         updateBallPosition();
         console.log(ballXPos + " " + ballYPos);
     }
@@ -35,7 +66,7 @@ function Game() {
 
     return (
         <div style={{
-            margin: 0,
+            margin: 20,
             display: "flex",
             justifyContent: "center",
             alignItem: "center"
