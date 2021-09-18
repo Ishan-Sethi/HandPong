8ea2d7ef3807bf3932ea424dffe52b0af724255d
Min <63823931+MuteMini@users.noreply.github.com>
2021-09-18 10:38:38 -0400

    testing

--- pages/hello.js
+++
@@ -1,7 +0,0 @@
-export default function Hello() {
-  return (
-    <div className={styles.container}>
-      
-    </div>
-  )
-}
--- pages/index.js
+++ pages/index.js
@@ -1,10 +1,11 @@
 import Image from 'next/image'
-import styles from '../styles/Home.module.css'
+import {} from '@chakra-ui/react'
+import { Center } from "@chakra-ui/react"
 
 export default function Home() {
   return (
-    <div className={styles.container}>
-      penis
-    </div>
+    <Center bg="brand.900" w="100%" h="100%" color="brand.400">
+      Hey
+    </Center>
   )
 }
