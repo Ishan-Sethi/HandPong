b7cbd8939e2f562dd6a842201d96a69d0994a197
Min <63823931+MuteMini@users.noreply.github.com>
2021-09-19 00:14:18 -0400

    Update socket.js

--- src/store/socket.js
+++ src/store/socket.js
@@ -1,5 +1,4 @@
 import socketClient from "socket.io-client";
-import { useHistory } from "react-router-dom";
 
 const SERVER = "http://192.168.1.171:8080";
 const socket = socketClient(SERVER, {transports: ['websocket']});
